203450b6d1c53b712ecc9ada32c115d979933651
SwissRoll <starpdec+git@gmail.com>
2019-03-27 00:19:06 -0400

    can't get js working

--- sponsors.js
+++ sponsors.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    display_sponsor_info()
+    display_sponsor_info();
 });
 
 function display_sponsor_info() {
-    $.post("sponsors.php",{type: "sponsor"}, function(data, status) {
-        $("#sponsors").html(data);
+    $.post("sponsors.php", function(data, status) {
+        $("#sponsors_list").html(data);
     });
 }
