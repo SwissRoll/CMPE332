2de5a29358d019d91fbdff19b71180e2973e8a77
SwissRoll <starpdec+git@gmail.com>
2019-03-27 00:57:56 -0400

    restoring jobs.php

--- jobs.php
+++ jobs.php
@@ -1,11 +1,23 @@
 <?php
+$companies = $_POST["companies"];
+if (is_null($companies) || !isset($companies) || empty($companies))
+{
     $pdo = new PDO('mysql:host=localhost;dbname=conference', "root", "");
-    $sql = "SELECT * FROM jobs";
+    $sql = "SELECT * FROM jobs;";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-
+    $stmt->execute(array($companies));
+    $out = "<div class=\"item\">
+        <div class=\"content\"> 
+            <table class=\"header\">
+                <tr>
+                <th>Company</th>
+                <th>Position</th>
+                <th>City</th>
+                <th>Province</th>
+                <th>Pay</th> 
+                </tr>";
     while ($row = $stmt->fetch()) {
-        echo "<tr>
+        $out .= "<tr>
                     <td>".$row["company"]."</td>
                     <td>".$row["name"]."</td>
                     <td>".$row["city"]."</td>
@@ -13,4 +25,35 @@
                     <td>".$row["pay"]."</td>
                 </tr>";
     }
+    $out .= "</table> </div> </div>";
+    echo $out;
+}
+else
+{
+    $pdo = new PDO('mysql:host=localhost;dbname=conference', "root", "");
+    $sql = "SELECT * FROM jobs where company=?;";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array($companies));
+    $out = "<div class=\"item\">
+        <div class=\"content\"> 
+            <table class=\"header\">
+                <tr>
+                <th>Company</th>
+                <th>Position</th>
+                <th>City</th>
+                <th>Province</th>
+                <th>Pay</th> 
+                </tr>";
+    while ($row = $stmt->fetch()) {
+        $out .= "<tr>
+                    <td>".$row["company"]."</td>
+                    <td>".$row["name"]."</td>
+                    <td>".$row["city"]."</td>
+                    <td>".$row["province"]."</td>
+                    <td>".$row["pay"]."</td>
+                </tr>";
+    }
+    $out .= "</table> </div> </div>";
+    echo $out;
+}
 ?>
