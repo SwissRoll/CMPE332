3a1693b48d8b4185b3707ad512dcc060ae7a13cd
Luke <luke.andrews@queensu.ca>
2019-03-26 23:38:15 -0400

    updated job postings:

--- jobs.php
+++ jobs.php
@@ -1,26 +1,11 @@
 <?php
-$companies = $_POST["companies"];
-
-if (is_null($companies) || !isset($companies) || empty($companies))
-{
     $pdo = new PDO('mysql:host=localhost;dbname=conference', "root", "");
-    $sql = "SELECT * FROM jobs;";
+    $sql = "SELECT * FROM jobs";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute(array($companies));
-
-    $out = "<div class=\"item\">
-        <div class=\"content\"> 
-            <table class=\"header\">
-                <tr>
-                <th>Company</th>
-                <th>Position</th>
-                <th>City</th>
-                <th>Province</th>
-                <th>Pay</th> 
-                </tr>";
+    $stmt->execute();
 
     while ($row = $stmt->fetch()) {
-        $out .= "<tr>
+        echo "<tr>
                     <td>".$row["company"]."</td>
                     <td>".$row["name"]."</td>
                     <td>".$row["city"]."</td>
@@ -28,39 +13,4 @@ if (is_null($companies) || !isset($companies) || empty($companies))
                     <td>".$row["pay"]."</td>
                 </tr>";
     }
-
-    $out .= "</table> </div> </div>";
-    echo $out;
-}
-else
-{
-    $pdo = new PDO('mysql:host=localhost;dbname=conference', "root", "");
-    $sql = "SELECT * FROM jobs where company=?;";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(array($companies));
-
-    $out = "<div class=\"item\">
-        <div class=\"content\"> 
-            <table class=\"header\">
-                <tr>
-                <th>Company</th>
-                <th>Position</th>
-                <th>City</th>
-                <th>Province</th>
-                <th>Pay</th> 
-                </tr>";
-
-    while ($row = $stmt->fetch()) {
-        $out .= "<tr>
-                    <td>".$row["company"]."</td>
-                    <td>".$row["name"]."</td>
-                    <td>".$row["city"]."</td>
-                    <td>".$row["province"]."</td>
-                    <td>".$row["pay"]."</td>
-                </tr>";
-    }
-
-    $out .= "</table> </div> </div>";
-    echo $out;
-}
 ?>
