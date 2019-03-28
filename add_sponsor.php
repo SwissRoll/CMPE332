3d214235410ee2e8d755cb68583dcebe10513900
SwissRoll <starpdec+git@gmail.com>
2019-03-28 15:01:23 -0400

    trying to get adding a sponsor working

---
+++ add_sponsor.php
@@ -0,0 +1,65 @@
+<?php
+$name = $_POST["name"];
+$tier = $_POST["tier"];
+$cost = $_POST["cost"];
+
+$pdo = new PDO('mysql:host=localhost;dbname=conference', "root", "");
+if ($type == "student") {
+    $sql = "SELECT hotel_rooms.roomNum FROM students right join hotel_rooms on students.roomNum = hotel_rooms.roomNum group by hotel_rooms.roomNum having"." COUNT(aID) <3 order by COUNT(aID) asc limit 1;";
+    $stmt = $pdo->prepare($sql);   
+    $stmt->execute();
+    $row = $stmt->fetch();
+    if ($row == false) {
+        echo "No free hotel rooms available, unable to add attendee";
+    } else {
+        $room = $row["roomNum"];
+    }
+
+    $sql = "INSERT INTO attendees (fname,lname,type,cost) VALUES ( ?, ?, \"student\", \"50\")";
+    $stmt = $pdo->prepare($sql);   
+    $stmt->execute(array($fname, $lname));
+
+    $sql = "SELECT aID FROM attendees ORDER by aID desc LIMIT 1;";
+    $stmt = $pdo->prepare($sql);   
+    $stmt->execute(array($fname, $lname));
+
+    $row = $stmt->fetch();
+
+    $aID = $row["aID"];
+
+    $sql = "INSERT INTO students VALUES ( ?, ?)";
+    $stmt = $pdo->prepare($sql);   
+    $stmt->execute(array($aID, $room));
+
+    echo "Successfully added attendee";
+
+} elseif ($type == "sponsor")  {
+    $sql = "INSERT INTO attendees (fname,lname,type,cost) VALUES ( ?, ?, \"sponsor\", \"0\")";
+    $stmt = $pdo->prepare($sql);   
+    $stmt->execute(array($fname, $lname));
+
+
+    $sql = "SELECT aID FROM attendees ORDER by AID desc LIMIT 1;";
+    $stmt = $pdo->prepare($sql);   
+    $stmt->execute(array($fname, $lname));
+
+    $row = $stmt->fetch();
+
+    $aID = $row["aID"];
+
+    $sql = "INSERT INTO sponsor_attendees VALUES ( ?, ?)";
+    $stmt = $pdo->prepare($sql);   
+    $stmt->execute(array($aID, $sponsor));
+
+    echo "Successfully added attendee";
+
+} else {
+    $sql = "INSERT INTO attendees (fname,lname,type,cost) VALUES ( ?, ?, \"professional\", \"100\")";
+    $stmt = $pdo->prepare($sql);   
+    $stmt->execute(array($fname, $lname));
+
+    echo "Successfully added attendee";
+}
+
+
+?>
--- sponsors.js
+++ sponsors.js
@@ -1,4 +1,32 @@
 $(document).ready(function(){
+	$('.ui.sidebar').sidebar();
+    $('.ui.accordion').accordion({
+        exclusive: false
+      });
+    $('.ui.dropdown').dropdown();
+	
+	// TO CHANGE
+	$("#submit_button").click(function () {
+		$.post("add_sponsor.php",
+			{
+                fname: $("#fname").val(),
+                lname: $("#lname").val(),
+                sponsor: $("#sponsor_dropdown").find(":selected").text(),
+                type:  $("#type_dropdown").find(":selected").text()
+            }, function (data,status) {
+                $("#response_content").text(data)
+                $('.ui.basic.modal').modal({
+                    closable  : false,
+                    onApprove : function() {
+                        display_attendees("student");
+                        display_attendees("sponsor");
+                        display_attendees("professional");
+                    }
+                  })
+                  .modal('show');
+            });
+        });
+	
     display_sponsor_info();
 });
 
@@ -6,4 +34,14 @@ function display_sponsor_info() {
 	$.get("sponsors.php", {sponsor_name: "sponsor"}, function(data, status) {
 	$("#sponsors_list").html(data);
 });
+}
+
+//TO CHANGE
+function get_sponsors() {
+    if ($("#type_dropdown").find(":selected").text() == "sponsor") {
+        $.get("sponsor_list.php", function(data,status) {
+            $("#sponsor_selection").html(data);
+        });
+        $('.ui.dropdown').dropdown();
+    } 
 }
